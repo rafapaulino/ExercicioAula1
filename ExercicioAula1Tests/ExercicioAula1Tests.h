4dbd7bb2486fcfcfe85e0a9d7f3d14337b7ce740
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-08-27 11:18:37 -0300

    Initial Commit

---
+++ ExercicioAula1Tests/ExercicioAula1Tests.h
@@ -0,0 +1,13 @@
+//
+//  ExercicioAula1Tests.h
+//  ExercicioAula1Tests
+//
+//  Created by Rafael Brigagão Paulino on 27/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ExercicioAula1Tests : SenTestCase
+
+@end
---
+++ ExercicioAula1Tests/ExercicioAula1Tests.m
@@ -0,0 +1,32 @@
+//
+//  ExercicioAula1Tests.m
+//  ExercicioAula1Tests
+//
+//  Created by Rafael Brigagão Paulino on 27/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import "ExercicioAula1Tests.h"
+
+@implementation ExercicioAula1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExercicioAula1Tests");
+}
+
+@end
