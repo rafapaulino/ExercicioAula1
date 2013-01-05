59df3ded6d44fd076a8345a37c8e27b7b0904e8c
Rafael Paulino <rafaclasses@gmail.com>
2013-01-05 17:06:46 -0200

    Exercicio aula 1

--- ExercicioAula1/PrincipalViewController.h
+++ ExercicioAula1/PrincipalViewController.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PrincipalViewController : UIViewController
+//protocolando o metodo <UITextFieldDelegate>
+//pois assim a classe te avisa quando tiver um erro
+//todavez que umaclasse vai atuar como delegate de outra devemos declarar um protocolo deste delegate conforme a linha abaixo. <Nome da classe de origem + Delegate>
+@interface PrincipalViewController : UIViewController <UITextFieldDelegate /*,UITableDelegate */>
+
+@property (nonatomic, weak) IBOutlet UILabel *minhaLabel;
+@property (nonatomic, weak) IBOutlet UITextField *meuTexto;
+
+-(IBAction)botaoClicado:(id)sender;
 
 @end
--- ExercicioAula1/PrincipalViewController.m
+++ ExercicioAula1/PrincipalViewController.m
@@ -14,6 +14,37 @@
 
 @implementation PrincipalViewController
 
+//metodo sera chamado toda vez que o textfiled receber o evento de clique no botao return
+//o textfield nao sabe o que fazer entao delega para a viewController executar alguma acao, definida neste metodo
+//nao esquecer de fazer a conexao no StoryBoard (delegate -> ViewController)
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"O return foi clicado a periferia agradece!");
+    
+    //neste caso quero que return execute a mesma coisa que o botao faria
+    [self botaoClicado:nil];
+    
+    //tanto faz o retorno para este metodo
+    return YES;
+}
+
+//metodo que altera o texto da label
+-(IBAction)botaoClicado:(id)sender
+{
+    //pegar o texto do botao
+    NSString *novoTexto = _meuTexto.text;
+    //alterar o texto do label
+    _minhaLabel.text = novoTexto;
+    //apagar o texto do campo de entrada de texto
+    _meuTexto.text = @"";
+    
+    //para fazer o teclado ser ocultado nos temos que fazer o campo de texto perder o foco
+    //firstresponder - ser o componente com o foco naquele momento. Resign faz o componente abrir mao do foco
+    [_meuTexto resignFirstResponder];
+    
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
